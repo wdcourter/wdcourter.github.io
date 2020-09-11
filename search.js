9887f1aa578672354910b1f1ca8922e984ade72c
Bill Courter <wdcourter@yahoo.com>
2020-09-10 17:46:54 -0700

    20200910v1

--- search.js
+++ search.js
@@ -1,23 +1,40 @@
-var compTIA = ["Mobile Devices", "Networking", "Hardware", "Virtualization and Cloud Computing", "Hardware and Network Troubleshooting", "Operating Systems", "Security",
-"Software Troubleshooting", "Operational Procedures", "Networking Concepts", "Infrastructure", "Network Operations", "Network Security", "Network Troubleshooting and Tools",
-"Threats, Attacks and Vulnerabilities", "Technologies and Tools", "Architecture and Design", "Identity and Access Management", "Risk Management", "Cryptography and PKI"
+// var compTIA = ["Mobile Devices", "Networking", "Hardware", "Virtualization and Cloud Computing", "Hardware and Network Troubleshooting", "Operating Systems", "Security",
+// "Software Troubleshooting", "Operational Procedures", "Networking Concepts", "Infrastructure", "Network Operations", "Network Security", "Network Troubleshooting and Tools",
+// "Threats, Attacks and Vulnerabilities", "Technologies and Tools", "Architecture and Design", "Identity and Access Management", "Risk Management", "Cryptography and PKI"]
   
-function displaySearch() {
- var displayResult = document.getElementById("displayResult");
-  window.history.pushState({},"Test","search.html");
- document.body.innerHTML = result;
+// function displaySearch() {
+//  var displayResult = document.getElementById("displayResult");
+//   window.history.pushState({},"Test","search.html");
+//  document.body.innerHTML = result;
+// }
+
+// function doSearch() {
+//  var searchField = document.getElementById("searchField").value;
+
+//  result=db.filter(.toUpperCase()===searchField.toUpperCase()).map().join("<br>");
+
+//  displaySearch();
+
+// }
+
+// function search ()  { 
+//      document.getElementById("searchButton").addEventListener("click", doSearch, false);
+
+// window.addEventListener("load", search, false);
+
+var storage = {
+    "networking": "netPlus.html",
+    "devices": "aPlus1.html",
+    "mobile": "aPlus1.html"
 }
 
-function doSearch() {
- var searchField = document.getElementById("searchField").value;
+function search(event){
+    event.preventDefault()
+    var searchQuery = document.getElementById('searchField').value.toLowerCase();
 
- result=db.filter(.toUpperCase()===searchField.toUpperCase()).map().join("<br>");
-
- displaySearch();
-
+    if(storage[searchQuery]) {
+        //redirect to the results of storage[searchQuery]
+        //window.history.pushState((), '', )
+        window.location = storage[searchQuery]
+    }
 }
-
-function search ()  { 
-     document.getElementById("searchButton").addEventListener("click", doSearch, false);
-
-window.addEventListener("load", search, false);
